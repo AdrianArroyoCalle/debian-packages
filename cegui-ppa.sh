787b96bff953d73fe5437c1f088258f38ed7f09d
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-09-14 17:51:47 +0200

    New scripts

---
+++ cegui-ppa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd cegui-ppa
+#debian/rules install-orig-source
+#dpkg-buildpackage -rfakeroot -kadrian.arroyocalle@gmail.com
+debian/rules get-orig-source
+debuild -S -sa -kadrian.arroyocalle@gmail.com
+cd ..
+dput ppa:adrian-arroyocalle/cegui *.changes
